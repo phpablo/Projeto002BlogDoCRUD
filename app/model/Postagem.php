da05ea4d531604299ad2992c14f5b1dda2efe1c8
phpablo <pablohenrique120@gmail.com>
2023-11-21 16:18:53 -0300

    :memo: Add .gitignore - File do banco de dados - Files default do gitignore segundo o chatGPT

--- app/model/Postagem.php
+++ app/model/Postagem.php
@@ -1,11 +1,9 @@
-<?php 
+<?php
 
-class Postagem  {
-  
-  public static function selecionaTodos(){
-    $con = new PDO('mysql: host=localhost; nome=blogdocrud;','root','');
+class Postagem {
+
+  public static function selecionaTodos() {
+    $con = new PDO('mysql: host=localhost; dbname=blogdocrud;', 'root', '');
     var_dump($con);
   }
 }
-
-?>
